86d4d270365944cf4c44f95131abd80ad30186e0
Wojciech Cywinski <wcywin@gmail.com>
2020-06-21 14:16:56 +0200

    React app Videos Search with Youtube Api

---
+++ src/components/VideoItem.js
@@ -0,0 +1,24 @@
+import React from "react"
+import './VideoItem.scss'
+
+const VideoItem = ({ video, onVideoSelect }) => {
+  return (
+    <div
+      className="c-video-item item"
+      onClick={() => onVideoSelect(video)}
+    >
+      <img
+        src={video.snippet.thumbnails.medium.url}
+        alt={video.snippet.title}
+        className="ui image"
+      />
+      <div className="content">
+        <div className="header">
+          {video.snippet.title}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default VideoItem
